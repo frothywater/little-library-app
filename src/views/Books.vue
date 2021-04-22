da328e333f7b90e7d7ade30c52cb15aa6a90e3b0
frothywater <kokoroic@outlook.com>
2021-04-22 21:00:40 +0800

    feature: filter dialog

--- src/views/Books.vue
+++ src/views/Books.vue
@@ -3,7 +3,11 @@
     <v-toolbar flat>
       <v-toolbar-title>Books</v-toolbar-title>
       <v-spacer />
-      <v-btn text color="secondary">
+      <v-btn
+        text
+        color="secondary"
+        @click="showFilterDialog = !showFilterDialog"
+      >
         <v-icon size="18" class="mr-2">mdi-filter</v-icon>
         Filter
       </v-btn>
@@ -11,16 +15,12 @@
 
     <book-table :books="books" />
 
-    <v-dialog v-model="dialog" max-width="500">
+    <v-dialog v-model="showAddBookDialog" max-width="500">
       <v-card>
         <v-card-title>Add Book</v-card-title>
 
         <v-card-text>
-          <v-form
-            v-model="validNewBook"
-            @submit.prevent="saveNewBook"
-            ref="form"
-          >
+          <v-form @submit.prevent="saveNewBook" ref="newBookForm">
             <v-row>
               <v-col cols="12" class="pb-0">
                 <v-text-field
@@ -101,6 +101,97 @@
       </v-card>
     </v-dialog>
 
+    <v-dialog v-model="showFilterDialog" max-width="500">
+      <v-card>
+        <v-card-title>Search</v-card-title>
+
+        <v-card-text>
+          <v-form @submit.prevent="saveNewBook" ref="filterForm">
+            <v-row>
+              <v-col cols="12" class="pb-0">
+                <v-text-field
+                  v-model="draftFilterParams.title"
+                  label="Title"
+                  maxlength="255"
+                />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="4" class="py-0">
+                <v-text-field
+                  v-model="draftFilterParams.author"
+                  label="Author"
+                  maxlength="255"
+                />
+              </v-col>
+              <v-col cols="4" class="py-0">
+                <v-text-field
+                  v-model="draftFilterParams.press"
+                  label="Press"
+                  maxlength="255"
+                />
+              </v-col>
+              <v-col cols="4" class="py-0">
+                <v-text-field
+                  v-model="draftFilterParams.category"
+                  label="Category"
+                  maxlength="255"
+                />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="3" class="py-0">
+                <v-text-field
+                  v-model="draftFilterParams.minYear"
+                  label="Min Year"
+                  type="number"
+                  min="0"
+                  :rules="[rules.nonNegative]"
+                />
+              </v-col>
+              <v-col cols="3" class="py-0">
+                <v-text-field
+                  v-model="draftFilterParams.maxYear"
+                  label="Max Year"
+                  type="number"
+                  min="0"
+                  :rules="[rules.nonNegative]"
+                />
+              </v-col>
+              <v-col cols="3" class="py-0">
+                <v-text-field
+                  v-model="draftFilterParams.minPrice"
+                  label="Min Price"
+                  type="number"
+                  min="0"
+                  step="0.1"
+                  :rules="[rules.nonNegative]"
+                />
+              </v-col>
+              <v-col cols="3" class="py-0">
+                <v-text-field
+                  v-model="draftFilterParams.maxPrice"
+                  label="Max Price"
+                  type="number"
+                  min="0"
+                  step="0.1"
+                  :rules="[rules.nonNegative]"
+                />
+              </v-col>
+            </v-row>
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="cancelFilter">Cancel</v-btn>
+          <v-btn color="primary" text type="submit" @click="applyFilter"
+            >Apply</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-fab-transition>
       <v-btn
         color="secondary"
@@ -108,7 +199,7 @@
         fab
         absolute
         class="fab-button"
-        @click="dialog = !dialog"
+        @click="showAddBookDialog = !showAddBookDialog"
       >
         <v-icon>mdi-plus</v-icon>
       </v-btn>
@@ -119,15 +210,15 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import BookTable from '@/components/BookTable.vue';
-import { BookRow, BookInfo } from 'little-library/src/typing';
+import { BookRow, BookInfo, BookSearchParams } from 'little-library/src/typing';
 import { InputValidationRule } from 'vuetify';
 
 @Component({
   components: { BookTable },
 })
 export default class Books extends Vue {
-  dialog = false;
-  validNewBook = false;
+  showAddBookDialog = false;
+  showFilterDialog = false;
 
   books: BookRow[] = [
     {
@@ -143,7 +234,9 @@ export default class Books extends Vue {
     },
   ];
 
-  draftBook: BookInfo = {
+  draftBook: BookInfo = Books.emptyBook;
+
+  static readonly emptyBook: BookInfo = {
     title: '',
     author: '',
     press: '',
@@ -153,33 +246,50 @@ export default class Books extends Vue {
     count: 10,
   };
 
-  readonly emptyBook: BookInfo = {
-    title: '',
-    author: '',
-    press: '',
-    category: '',
-    year: new Date().getFullYear(),
-    price: 10.0,
-    count: 10,
-  };
+  draftFilterParams: BookFilterParams = {};
 
   rules: { [key: string]: InputValidationRule } = {
     required: (value) => !!value || 'Required.',
-    nonNegative: (value) => value >= 0 || 'Should be no less than 0.',
+    nonNegative: (value) => !value || value >= 0 || 'Should be no less than 0.',
   };
 
   saveNewBook(): void {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if ((this.$refs.form as any).validate()) {
-      this.dialog = false;
+    if ((this.$refs.newBookForm as any).validate()) {
+      this.showAddBookDialog = false;
     }
   }
 
   cancelNewBook(): void {
-    Object.assign(this.draftBook, this.emptyBook);
+    Object.assign(this.draftBook, Books.emptyBook);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (this.$refs.form as any).resetValidation();
-    this.dialog = false;
+    (this.$refs.newBookForm as any).resetValidation();
+    this.showAddBookDialog = false;
   }
+
+  applyFilter(): void {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if ((this.$refs.filterForm as any).validate()) {
+      this.showFilterDialog = false;
+    }
+  }
+
+  cancelFilter(): void {
+    this.draftFilterParams = {};
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (this.$refs.filterForm as any).resetValidation();
+    this.showFilterDialog = false;
+  }
+}
+
+interface BookFilterParams {
+  title?: string;
+  author?: string;
+  press?: string;
+  category?: string;
+  minYear?: number;
+  maxYear?: number;
+  minPrice?: number;
+  maxPrice?: number;
 }
 </script>
